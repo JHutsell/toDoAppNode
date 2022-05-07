e0eba41c529f550da2b29b139f292b9612562bbb
Justin Hutsell <hutselljustin@gmail.com>
2022-05-07 17:25:07 -0500

    adds ability to delete an entry in the database, again using axios

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,16 @@
 document.addEventListener("click", (e) => {
+  // delete feature
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Delete item permanently?")) {
+      axios.post('/delete-item', {id: e.target.getAttribute("data-id")}).then(() => {
+        e.target.parentElement.parentElement.remove() 
+      }).catch(() => {
+        alert("Please try again later")
+      })
+    }
+  }
+  
+  // update feature
   if (e.target.classList.contains("edit-me")) {
     let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
     if (userInput) {
