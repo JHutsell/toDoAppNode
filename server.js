1b71154c69358cf637023f27024d4a8eeb840f8f
Justin Hutsell <hutselljustin@gmail.com>
2022-05-06 19:03:53 -0500

    adds ability to edit items using axios.js for async calls

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 let express = require('express')
-let {MongoClient} = require('mongodb')
+let {MongoClient, ObjectId} = require('mongodb')
 //destructuring
 
 let app = express()
 let db
+
+app.use(express.static('public'))
 
 async function start() {
   let client = new MongoClient('mongodb+srv://toDoAppUser:1234@cluster0.kiyed.mongodb.net/TodoApp?retryWrites=true&w=majority')
@@ -14,6 +16,7 @@ async function start() {
 
 start()
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
@@ -44,7 +47,7 @@ app.get('/', (req, res) => {
           return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
           <span class="item-text">${item.text}</span>
           <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+            <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
             <button class="delete-me btn btn-danger btn-sm">Delete</button>
           </div>
         </li>`
@@ -52,7 +55,9 @@ app.get('/', (req, res) => {
       </ul>
       
     </div>
-    
+
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>  
+  <script src="/browser.js"></script>
   </body>
   </html>`)
   })
@@ -62,6 +67,12 @@ app.get('/', (req, res) => {
 app.post('/create-item', (req, res) => {
   db.collection('items').insertOne({text: req.body.item}, () => {
     res.redirect('/')
+  })
+})
+
+app.post('/update-item', (req, res) => {
+  db.collection('items').findOneAndUpdate({_id: new ObjectId(req.body.id)}, {$set: {text: req.body.text}}, () => {
+    res.send("SUCESS")
   })
 })
 
